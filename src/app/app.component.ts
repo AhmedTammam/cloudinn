5a51731659d4295fd652ef11a7e20e6d4f583b41
AhmedTammam <tamam65@gmail.com>
2018-05-05 07:24:24 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'app';
+
+  constructor(private httpClient:HttpClient){
+
+  }
+
+  term = '';
+  persons = [];
+
+  fetchData(value){
+   this.httpClient.get(`https://swapi.co/api/people/?search=${value.term}`)
+   .subscribe(
+     (data)=>{
+         this.persons = data.results;
+       console.log(this.persons);
+       
+       
+     }
+   )
+    
+  }
+  
 }
